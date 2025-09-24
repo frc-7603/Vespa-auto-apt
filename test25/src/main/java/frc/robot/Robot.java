28e4552f0c4f264170ccb757bd37773afd823abb
L1F3atbhss <Nathan.chan3000@gmail.com>
2025-09-24 14:01:52 -0400

    working code

--- test25/src/main/java/frc/robot/Robot.java
+++ test25/src/main/java/frc/robot/Robot.java
@@ -6,24 +6,26 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import edu.wpi.first.wpilibj.Joystick;
+
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
 
-/**
- * Simple FRC robot program using CAN VictorSPX motor controllers for tank drive.
- */
 public class Robot extends TimedRobot {
-  // CAN IDs from Phoenix Tuner
-  private final VictorSPX motorL1 = new VictorSPX(10);
-  private final VictorSPX motorL2 = new VictorSPX(7);
-  private final VictorSPX motorR1 = new VictorSPX(5);
-  private final VictorSPX motorR2 = new VictorSPX(6);
 
-  // Single joystick (gamepad)
-  private final Joystick stick = new Joystick(0);
+  // CAN IDs
+  private final VictorSPX motorL2 = new VictorSPX(1);
+  private final VictorSPX motorL1 = new VictorSPX(2);
+
+  private final VictorSPX motorR2 = new VictorSPX(3);
+  private final VictorSPX motorR1 = new VictorSPX(4);
+
+
+  // joystick
+  private final GenericHID stick = new GenericHID(0);
 
   @Override
   public void teleopPeriodic() {
+
     // Tank drive with joystick axes
     double leftSpeed = -stick.getRawAxis(1);  // left stick Y
     double rightSpeed = -stick.getRawAxis(5); // right stick Y
@@ -35,4 +37,10 @@ public class Robot extends TimedRobot {
     motorR1.set(ControlMode.PercentOutput, rightSpeed);
     motorR2.set(ControlMode.PercentOutput, rightSpeed);
   }
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
 }
