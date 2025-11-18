e51eaee33dff9ec21b690d56427e1aa56a69d610
L1F3atbhss <Nathan.chan3000@gmail.com>
2025-11-18 13:14:20 -0500

    code for the 2023 bot (in 2025)

---
+++ 2023/bot2023/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class RobotContainer {
+  private final VictorSPX motorL1 = new VictorSPX(1);
+  private final VictorSPX motorL2 = new VictorSPX(1);
+  private final VictorSPX motorR1 = new VictorSPX(1);
+  private final VictorSPX motorR2 = new VictorSPX(1);
+
+  private final GenericHID stick = new GenericHID(0);
+
+  public RobotContainer() {
+    configureBindings();
+
+     // Forward/backward from left stick X (axis 0)
+    double forward = -stick.getRawAxis(0) / 3;
+
+    // Rotation from right stick Y (axis 5) 
+    double rotation = stick.getRawAxis(5) / 3;
+
+    // Combine for arcade drive
+    double leftSpeed = forward + rotation;
+    double rightSpeed = forward - rotation;
+
+    // Send outputs to motors
+    motorL1.set(ControlMode.PercentOutput, leftSpeed);
+    motorL2.set(ControlMode.PercentOutput, leftSpeed);
+
+    motorR1.set(ControlMode.PercentOutput, rightSpeed);
+    motorR2.set(ControlMode.PercentOutput, rightSpeed);
+    //right stick = turn
+    //left stick = move
+  }
+
+  private void configureBindings() {}
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
