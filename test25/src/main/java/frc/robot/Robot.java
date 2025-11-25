3daaf2c1c7324e2b3e52ce8680946377e260f783
L1F3 <Nathan.chan3000@gmail.com>
2025-11-25 12:24:44 -0500

    Refactor motor IDs and implement shooter controls (2024 bot code)
    Updated motor CAN IDs and added shooter control logic.

--- test25/src/main/java/frc/robot/Robot.java
+++ test25/src/main/java/frc/robot/Robot.java
@@ -6,42 +6,68 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
-  // CAN IDs
-  private final VictorSPX motorL2 = new VictorSPX(1);
-  private final VictorSPX motorL1 = new VictorSPX(2);
+  // CAN IDs for drivetrain
+  private final VictorSPX motorL2 = new VictorSPX(2);
+  private final VictorSPX motorL1 = new VictorSPX(1);
+  private final VictorSPX motorR2 = new VictorSPX(8);
+  private final VictorSPX motorR1 = new VictorSPX(9);
 
-  private final VictorSPX motorR2 = new VictorSPX(3);
-  private final VictorSPX motorR1 = new VictorSPX(4);
+  // CAN IDs for shooter
+  private final VictorSPX bottomShooter = new VictorSPX(7);  
+  private final VictorSPX topShooter = new VictorSPX(10);
 
+  // Joystick
+  private final Joystick stick = new Joystick(0);
 
-  // joystick
-  private final GenericHID stick = new GenericHID(0);
+  // Shooter button mappings
+  private final int intakeButton = 5;
+  private final int shootButton  = 6;
+  private final int revButton    = 7;
+
+  // Shooter speeds
+  private final double intakespeed = 0.67;   // adjust as needed
+  private final double firingspeed = -1;  // negative if needed for outtake
 
   @Override
   public void teleopPeriodic() {
-  // Forward/backward from left stick X (axis 0)
-  double forward = -stick.getRawAxis(0) / 3;
+    // Drivetrain Control 
+    double forward = stick.getRawAxis(0) / 3;   // left stick X
+    double rotation = -stick.getRawAxis(5) / 3;   // right stick Y
 
-  // Rotation from right stick Y (axis 5) 
-  double rotation = stick.getRawAxis(5) / 3;
+    double leftSpeed = forward + rotation;
+    double rightSpeed = forward - rotation;
 
-  // Combine for arcade drive
-  double leftSpeed = forward + rotation;
-  double rightSpeed = forward - rotation;
+    motorL1.set(ControlMode.PercentOutput, leftSpeed);
+    motorL2.set(ControlMode.PercentOutput, leftSpeed);
+    motorR1.set(ControlMode.PercentOutput, rightSpeed);
+    motorR2.set(ControlMode.PercentOutput, rightSpeed);
 
-  // Send outputs to motors
-  motorL1.set(ControlMode.PercentOutput, leftSpeed);
-  motorL2.set(ControlMode.PercentOutput, leftSpeed);
-
-  motorR1.set(ControlMode.PercentOutput, rightSpeed);
-  motorR2.set(ControlMode.PercentOutput, rightSpeed);
-  //right stick = turn
-  //left stick = move
+    // Shooter Control 
+    if (stick.getRawButton(intakeButton)) {
+      // Intake: pull game piece in
+      bottomShooter.set(ControlMode.PercentOutput, intakespeed);
+      topShooter.set(ControlMode.PercentOutput, intakespeed);
+    } 
+    else if (stick.getRawButton(shootButton)) {
+      // Shoot/outtake
+      bottomShooter.set(ControlMode.PercentOutput, firingspeed);
+      topShooter.set(ControlMode.PercentOutput, firingspeed);
+    } 
+  else if (stick.getRawButton(revButton)) {
+      // Rev BOTH shooters
+      bottomShooter.set(ControlMode.PercentOutput, firingspeed);
+      topShooter.set(ControlMode.PercentOutput, firingspeed);
+    } 
+  else {
+    // Stop shooters
+    bottomShooter.set(ControlMode.PercentOutput, 0);
+    topShooter.set(ControlMode.PercentOutput, 0);
+  }
 }
 
   @Override
